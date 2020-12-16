03f0236bf4e98a6d4925267475dd07e523bc1fd0
b3z <b3z@zeppel.eu>
2020-12-16 13:25:41 +0100

    fix #6

--- src/database.ts
+++ src/database.ts
@@ -7,45 +7,55 @@ import { log } from "./logger";
 export class Database {
     private client: any;
 
-
     public constructor() {
         const port: number = config.get("server.port");
         const host: string = config.get("server.host");
-        this.client = redis.createClient(port, host); 
-        this.client.on("connect", function () { 
-            log.debug("Connected to redis server: "+host+":"+port);
+        this.client = redis.createClient(port, host);
+        this.client.on("connect", function () {
+            log.debug("Connected to redis server: " + host + ":" + port);
         });
         redisearch(redis);
-        this.client.ft_create("index STOPWORDS 0 SCHEMA data TEXT".split(" "), (err: Error) => {
-            if (err.message == "Index already exists") // check for schema already existent
-                {log.debug(err.message)}
-            else
-                {log.error(err);}
-        });
+        this.client.ft_create(
+            "index STOPWORDS 0 SCHEMA data TEXT".split(" "),
+            (err: Error) => {
+                if (err.message == "Index already exists") {
+                    // check for schema already existent
+                    log.debug(err.message);
+                } else {
+                    log.error(err);
+                }
+            }
+        );
     }
 
     public save(data: string): any {
         // adding to index: docID = hash, score = 1.0 with FILEDS data = the date we pass.
-        this.client.ft_add(["index", md5(data), "1.0", "FIELDS", "data", data], (err: Error) => {
-            log.error(err);
-        });
+        this.client.ft_add(
+            ["index", md5(data), "1.0", "FIELDS", "data", data],
+            (err: Error) => {
+                log.error(err);
+            }
+        );
 
         log.debug(md5(data));
     }
 
     public async search(query: string): Promise<string[]> {
         return new Promise<string[]>((resolve) => {
-
             // token escaping --> https://oss.redislabs.com/redisearch/Escaping.html
 
-            query = query.replace(/:/g, "\\:"); // escape ':'
-            log.debug(this.client.ft_explain)
-            this.client.ft_search(["index", query], (err: Error, result: any) => {
-                if (err)
-                    {log.error(err);}
-                
-                resolve(result);
-            });
+            query = query.replace(/:/g, "\\:"); // escape ':' // bad fix for #6 - works but is not good
+            log.debug(this.client.ft_explain);
+            this.client.ft_search(
+                ["index", query],
+                (err: Error, result: any) => {
+                    if (err) {
+                        log.error(err);
+                    }
+
+                    resolve(result);
+                }
+            );
         });
     }
     //TODO for now returns nothing TODO make async
@@ -57,7 +67,9 @@ export class Database {
     // }
 
     public exists(key: string) {
-        if (key == null || key.length == 0) {throw new Error("Illegal Argument.");}
+        if (key == null || key.length == 0) {
+            throw new Error("Illegal Argument.");
+        }
 
         this.client.exists(key, function (err: string, reply: any) {
             if (reply === 1) {
