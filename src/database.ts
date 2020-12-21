b37cc727066cbd35524fe7461356b5bcacc581e7
b3z <b3z@zeppel.eu>
2020-12-21 23:02:59 +0100

    fix #13 temporarily

--- src/database.ts
+++ src/database.ts
@@ -33,6 +33,10 @@ export class Database {
 
     public save(data: string): any {
         // adding to index: docID = hash, score = 1.0 with FILEDS data = the date we pass.
+
+        // replace linebreak with space + linebreak (https://github.com/b3z/reventlou/issues/13#issuecomment-749061225)
+        data = data.replace(/\n/g, " \n"); // TODO fix this (#13) in a better way as soon as there is a solution from RediSearch/RediSearch#1749
+
         this.client.ft_add(
             [
                 "index",
@@ -56,6 +60,7 @@ export class Database {
         return new Promise<string[]>((resolve) => {
             // token escaping --> https://oss.redislabs.com/redisearch/Escaping.html
             query = query.replace(/:/g, "\\:"); // escape ':' // bad fix for #6 - works but is not good
+
             this.client.ft_search(
                 // ft.search index @data:(test) RETURN 1 data SORTBY timestamp
                 [
