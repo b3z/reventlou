f8ed2faed6d3575edce5547cd86ffd3b363d99d7
b3z <b3z@zeppel.eu>
2021-02-09 22:30:45 +0100

    md raw links

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,10 +1,18 @@
-const httpLinks = /\[([^[]+)\]\(([^)]+)\)/gim;
+const httpBracketLink = /\[([^[]+)\]\(([^)]+)\)/gim; // matches [text](url) note still needs https to be clickable
+const httpRawLink = /((?<!href=")https?:\/\/[^\s]+)/gim; // old raw match: /(https?:\/\/[^\s]+)/gim; // matches raw url without href=" in front
 
 const rules = {
-    links: {
-        regex: httpLinks,
+    httpBracketLink: {
+        regex: httpBracketLink,
         replacer: function (match, $1, $2) {
             return `<a href="${$2}">${$1}</a>`;
+        },
+    },
+    // matches all urls that don't have a href yet in front.
+    httpRawLink: {
+        regex: httpRawLink,
+        replacer: function (match, $1) {
+            return `<a href="${$1}">${$1}</a>`;
         },
     },
 };
